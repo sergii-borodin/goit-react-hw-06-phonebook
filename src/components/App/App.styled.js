acf775345134190e0525cd26e49700236e7c0dda
BoroDinS <sergey.borodin.lg@gmail.com>
2023-05-16 17:40:15 +0200

    make a little more responsive

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,13 +1,17 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 export const MainContainer = styled.div`
-margin: 0 auto;
-margin-top: 25px;
-display: flex;
-flex-direction: column;
-align-items: center;
+  margin: 0 auto;
+  margin-top: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 600px;
   border-radius: 15px;
   border: none;
-  background-color: rgba(255,255,255,1);
+  background-color: rgba(255, 255, 255, 1);
+  @media screen and (max-width: 660px) {
+    width: auto;
+    margin: 0 100px;
+  }
 `;
